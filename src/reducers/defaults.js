ac1f74f42993773927f649fe7acfa09a7580b967
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-05-03 20:37:38 +0200

    Add Eirini team

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -6,31 +6,36 @@ export const defaultState = {
     "0": [],
     "1": [],
     "2": [],
-    "out": ["will"],
-    "pm": ["julz"],
+    "out": [],
+    "pm": ["DrJulz", "Andrew", "Simon"],
     "unassigned": [
-       "ed", "kk", "craig", "roberto","tom",
+       "Julz", "Georgi", "Mario", "Steffen"
     ],
   },
   tracks: [
-    "0", "1", "2", "3", "4",
+    "0", "1", "2",
   ],
   trackNames: {
-    "0": "Track One",
-    "1": "Track Two",
-    "2": "Track Three",
+    "0": "",
+    "1": "",
+    "1": "",
   },
   photos: {
-    "julz": "https://avatars2.githubusercontent.com/u/354013?v=3&s=72",
-    "cool will": "https://avatars2.githubusercontent.com/u/1611510?v=3&s=72",
-    "ed": "https://avatars3.githubusercontent.com/u/6475144?v=3&s=72",
-    "kk": "https://avatars1.githubusercontent.com/u/1573630?v=3&s=72",
-    "craig": "https://avatars2.githubusercontent.com/u/4772216?v=3&s=72",
-    "roberto": "https://avatars2.githubusercontent.com/u/1674893?v=3&s=72",
-    "tom": "https://avatars2.githubusercontent.com/u/16281740?v=3&s=72",
-    "will": "https://avatars1.githubusercontent.com/u/1255755?v=3&s=72",
+    "DrJulz": "https://avatars0.githubusercontent.com/u/354013?s=96&v=4",
+    "Andrew": "https://avatars1.githubusercontent.com/u/12702547?s=96&v=4",
+    "Simon": "https://avatars1.githubusercontent.com/u/13646313?s=96&v=4",
+    "Julz": "https://avatars2.githubusercontent.com/u/25896939?s=96&v=4",
+    "Georgi": "https://avatars2.githubusercontent.com/u/15929230?s=96&v=4",
+    "Mario": "https://avatars1.githubusercontent.com/u/15073922?s=96&v=4",
+    "Steffen": "https://avatars0.githubusercontent.com/u/80507?s=96&v=4",
   },
-  badges: { "1": [ "CI" ] },
-  locked: { "julz": true },
+  badges: {
+    // "1": [ "CI" ]
+  },
+  locked: {
+    "DrJulz": true,
+    "Simon": true,
+    "Andrew": true,
+  },
   version: 0,
 }
